d24a07ac1c64e6b9867249fe8de4cfa219e1a7f0
Hayden Shively <haydenshively@users.noreply.github.com>
2019-10-30 14:50:30 -0700

    Added capsule_network files

---
+++ Capsule_Network/capsulenet.py
@@ -0,0 +1,239 @@
+"""
+Keras implementation of CapsNet in Hinton's paper Dynamic Routing Between Capsules.
+The current version maybe only works for TensorFlow backend. Actually it will be straightforward to re-write to TF code.
+Adopting to other backends should be easy, but I have not tested this. 
+
+Usage:
+       python capsulenet.py
+       python capsulenet.py --epochs 50
+       python capsulenet.py --epochs 50 --routings 3
+       ... ...
+       
+Result:
+    Validation accuracy > 99.5% after 20 epochs. Converge to 99.66% after 50 epochs.
+    About 110 seconds per epoch on a single GTX1070 GPU card
+    
+Author: Xifeng Guo, E-mail: `guoxifeng1990@163.com`, Github: `https://github.com/XifengGuo/CapsNet-Keras`
+"""
+
+import numpy as np
+import tensorflow as tf
+from keras import layers, models, optimizers, losses
+from keras import backend as K
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+from utils import combine_images
+#from PIL import Image
+from capsulelayers import CapsuleLayer, PrimaryCap, Length, Mask
+
+K.set_image_data_format('channels_last')
+
+
+def CapsNet(input_shape, n_class, routings):
+    """
+    A Capsule Network on MNIST.
+    :param input_shape: data shape, 3d, [width, height, channels]
+    :param n_class: number of classes
+    :param routings: number of routing iterations
+    :return: Two Keras Models, the first one used for training, and the second one for evaluation.
+            `eval_model` can also be used for training.
+    """
+    x = layers.Input(shape=input_shape)
+
+    # Layer 1: Just a conventional Conv2D layer
+    conv1 = layers.Conv2D(filters=32, kernel_size=3, strides=2, padding='valid', activation='relu', name='conv1')(x)
+    conv2 = layers.Conv2D(filters=64, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv2')(conv1)
+    conv3 = layers.Conv2D(filters=128, kernel_size=9, strides=1, padding='valid', activation='relu', name='conv3')(conv2)
+    conv4 = layers.Conv2D(filters=256, kernel_size=3, strides=1, padding='valid', activation='relu', name='conv4')(conv3)
+
+    # Layer 2: Conv2D layer with `squash` activation, then reshape to [None, num_capsule, dim_capsule]
+    primarycaps = PrimaryCap(conv4, dim_capsule=16, n_channels=16, kernel_size=15, strides=2, padding='valid')
+
+    # Layer 3: Capsule layer. Routing algorithm works here.
+    digitcaps = CapsuleLayer(num_capsule=21, dim_capsule=2, routings=4, name='digitcaps')(primarycaps)
+
+    train_model = models.Model([x], [digitcaps])
+    return train_model
+
+
+def margin_loss(y_true, y_pred):
+    """
+    Margin loss for Eq.(4). When y_true[i, :] contains not just one `1`, this loss should work too. Not test it.
+    :param y_true: [None, n_classes]
+    :param y_pred: [None, num_capsule]
+    :return: a scalar loss value.
+    """
+    L = y_true * K.square(K.maximum(0., 0.9 - y_pred)) + \
+        0.5 * (1 - y_true) * K.square(K.maximum(0., y_pred - 0.1))
+
+    return K.mean(K.sum(L, 1))
+
+
+def train(model, data, args):
+    """
+    Training a CapsuleNet
+    :param model: the CapsuleNet model
+    :param data: a tuple containing training and testing data, like `((x_train, y_train), (x_test, y_test))`
+    :param args: arguments
+    :return: The trained model
+    """
+    # unpacking the data
+    (x_train, y_train), (x_test, y_test) = data
+
+    # callbacks
+    log = callbacks.CSVLogger(args.save_dir + '/log.csv')
+    tb = callbacks.TensorBoard(log_dir=args.save_dir + '/tensorboard-logs',
+                               batch_size=args.batch_size, histogram_freq=int(args.debug))
+    checkpoint = callbacks.ModelCheckpoint(args.save_dir + '/weights-{epoch:02d}.h5', save_weights_only=True, verbose=1)
+    #lr_decay = callbacks.LearningRateScheduler(schedule=lambda epoch: args.lr * (args.lr_decay ** epoch))
+
+    def get_huber_loss_fn(**huber_loss_kwargs):
+        def custom_huber_loss(y_true, y_pred):
+            return tf.losses.huber_loss(y_true, y_pred, **huber_loss_kwargs)
+        return custom_huber_loss
+
+    def my_metric(y_true, y_pred):
+        return 88*K.mean(K.abs(y_pred - y_true))
+
+    # compile the model
+    model.compile(optimizer=optimizers.Adam(lr=args.lr),
+                  loss=['mse'],
+                  loss_weights=[1.0],
+                  metrics=[my_metric])
+
+
+    # Training without data augmentation:
+    model.fit(x=x_train, y=y_train, batch_size=args.batch_size, epochs=args.epochs,
+              validation_data=(x_test, y_test), callbacks=[log, tb, checkpoint], verbose=1)#, lr_decay])
+
+    model.save_weights(args.save_dir + '/trained_model.h5')
+    print('Trained model saved to \'%s/trained_model.h5\'' % args.save_dir)
+
+    from utils import plot_log
+    plot_log(args.save_dir + '/log.csv', show=True)
+
+    return model
+
+
+def test(model, data, args):
+    x_test, y_test = data
+    y_pred, x_recon = model.predict(x_test, batch_size=100)
+    print('-'*30 + 'Begin: test' + '-'*30)
+    print('Test acc:', np.sum(np.argmax(y_pred, 1) == np.argmax(y_test, 1))/y_test.shape[0])
+
+    img = combine_images(np.concatenate([x_test[:50],x_recon[:50]]))
+    image = img * 255
+    #Image.fromarray(image.astype(np.uint8)).save(args.save_dir + "/real_and_recon.png")
+    print()
+    print('Reconstructed images are saved to %s/real_and_recon.png' % args.save_dir)
+    print('-' * 30 + 'End: test' + '-' * 30)
+    plt.imshow(plt.imread(args.save_dir + "/real_and_recon.png"))
+    plt.show()
+
+
+def manipulate_latent(model, data, args):
+    print('-'*30 + 'Begin: manipulate' + '-'*30)
+    x_test, y_test = data
+    index = np.argmax(y_test, 1) == args.digit
+    number = np.random.randint(low=0, high=sum(index) - 1)
+    x, y = x_test[index][number], y_test[index][number]
+    x, y = np.expand_dims(x, 0), np.expand_dims(y, 0)
+    noise = np.zeros([1, 10, 16])
+    x_recons = []
+    for dim in range(16):
+        for r in [-0.25, -0.2, -0.15, -0.1, -0.05, 0, 0.05, 0.1, 0.15, 0.2, 0.25]:
+            tmp = np.copy(noise)
+            tmp[:,:,dim] = r
+            x_recon = model.predict([x, y, tmp])
+            x_recons.append(x_recon)
+
+    x_recons = np.concatenate(x_recons)
+
+    img = combine_images(x_recons, height=16)
+    image = img*255
+    #Image.fromarray(image.astype(np.uint8)).save(args.save_dir + '/manipulate-%d.png' % args.digit)
+    print('manipulated result saved to %s/manipulate-%d.png' % (args.save_dir, args.digit))
+    print('-' * 30 + 'End: manipulate' + '-' * 30)
+
+
+def load_mnist():
+    # the data, shuffled and split between train and test sets
+    from keras.datasets import mnist
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    #data = np.load('../MNIST_style.npy')
+
+    x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    x_test = x_test.reshape(-1, 28, 28, 1).astype('float32') / 255.
+    y_train = to_categorical(y_train.astype('float32'))
+    y_test = to_categorical(y_test.astype('float32'))
+    return (x_train, y_train), (x_test, y_test)
+
+def load_rhd():
+    X = np.load('/hdd/datasets/RHD_published_v2/training/MNIST-style-images.npy')
+    Y = np.load('/hdd/datasets/RHD_published_v2/training/MNIST-style-coords.npy')
+
+    X = X.reshape(-1, 88, 88, 1).astype('float32')/255.0
+    print(X.shape)
+    Y = Y.astype('float32')/88.0
+    print(Y.shape)
+
+    X_train = X[:X.shape[0]*2//3]
+    Y_train = Y[:Y.shape[0]*2//3]
+    X_test = X[X.shape[0]*2//3:]
+    Y_test = Y[Y.shape[0]*2//3:]
+
+    return (X_train, Y_train), (X_test, Y_test)
+
+
+if __name__ == "__main__":
+    import os
+    import argparse
+    from keras.preprocessing.image import ImageDataGenerator
+    from keras import callbacks
+
+    # setting the hyper parameters
+    parser = argparse.ArgumentParser(description="Capsule Network on MNIST.")
+    parser.add_argument('--epochs', default=25, type=int)
+    parser.add_argument('--batch_size', default=64, type=int)
+    parser.add_argument('--lr', default=0.01, type=float,
+                        help="Initial learning rate")
+    parser.add_argument('--lr_decay', default=0.9, type=float,
+                        help="The value multiplied by lr at each epoch. Set a larger value for larger epochs")
+    parser.add_argument('--lam_recon', default=0.392, type=float,
+                        help="The coefficient for the loss of decoder")
+    parser.add_argument('-r', '--routings', default=3, type=int,
+                        help="Number of iterations used in routing algorithm. should > 0")
+    parser.add_argument('--shift_fraction', default=0.1, type=float,
+                        help="Fraction of pixels to shift at most in each direction.")
+    parser.add_argument('--debug', action='store_true',
+                        help="Save weights by TensorBoard")
+    parser.add_argument('--save_dir', default='./result')
+    parser.add_argument('-t', '--testing', action='store_true',
+                        help="Test the trained model on testing dataset")
+    parser.add_argument('--digit', default=5, type=int,
+                        help="Digit to manipulate")
+    parser.add_argument('-w', '--weights', default=None,
+                        help="The path of the saved weights. Should be specified when testing")
+    args = parser.parse_args()
+    print(args)
+
+    if not os.path.exists(args.save_dir):
+        os.makedirs(args.save_dir)
+
+    # load data
+    (x_train, y_train), (x_test, y_test) = load_rhd()
+
+    # define model
+    model = CapsNet(input_shape=x_train.shape[1:], n_class=21, routings=args.routings)
+    model.summary()
+
+    # train or test
+    if args.weights is not None:  # init the model weights with provided one
+        model.load_weights(args.weights)
+    if not args.testing:
+        train(model=model, data=((x_train, y_train), (x_test, y_test)), args=args)
+    else:  # as long as weights are given, will run testing
+        if args.weights is None:
+            print('No weights are provided. Will test using random initialized weights.')
+        manipulate_latent(manipulate_model, (x_test, y_test), args)
+        test(model=eval_model, data=(x_test, y_test), args=args)
