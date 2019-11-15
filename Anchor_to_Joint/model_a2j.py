805b8e93a3b3b598bf3ff6635be875ce56904644
Hayden Shively <haydenshively@gmail.com>
2019-11-15 03:51:19 -0800

    Fixed resnet bugs (wayyyyy too many params before)

--- Anchor_to_Joint/model_a2j.py
+++ Anchor_to_Joint/model_a2j.py
@@ -13,23 +13,23 @@ class A2J_Regression(Model):
 
     def build(self):
         # input should be channels last
-        conv1 = layers.Conv2D(filters=self.feature_size, kernel_size=3, padding='valid', kernel_initializer='glorot_normal')(self.input_tensor)
+        conv1 = layers.Conv2D(filters=self.feature_size, kernel_size=3, padding='same', kernel_initializer='glorot_normal')(self.input_tensor)
         bn1 = layers.BatchNormalization(axis=3)(conv1)# , kernel_initializer='ones', bias_initializer='zeros'
         act1 = layers.ReLU()(bn1)
 
-        conv2 = layers.Conv2D(filters=self.feature_size, kernel_size=3, padding='valid', kernel_initializer='glorot_normal')(act1)
+        conv2 = layers.Conv2D(filters=self.feature_size, kernel_size=3, padding='same', kernel_initializer='glorot_normal')(act1)
         bn2 = layers.BatchNormalization(axis=3)(conv2)
         act2 = layers.ReLU()(bn2)
 
-        conv3 = layers.Conv2D(filters=self.feature_size, kernel_size=3, padding='valid', kernel_initializer='glorot_normal')(act2)
+        conv3 = layers.Conv2D(filters=self.feature_size, kernel_size=3, padding='same', kernel_initializer='glorot_normal')(act2)
         bn3 = layers.BatchNormalization(axis=3)(conv3)
         act3 = layers.ReLU()(bn3)
 
-        conv4 = layers.Conv2D(filters=self.feature_size, kernel_size=3, padding='valid', kernel_initializer='glorot_normal')(act3)
+        conv4 = layers.Conv2D(filters=self.feature_size, kernel_size=3, padding='same', kernel_initializer='glorot_normal')(act3)
         bn4 = layers.BatchNormalization(axis=3)(conv4)
         act4 = layers.ReLU()(bn4)
 
-        conv5 = layers.Conv2D(filters=self.num_anchors*self.num_classes*self.output_dims, kernel_size=3, padding='valid', kernel_initializer='glorot_normal')(act4)
+        conv5 = layers.Conv2D(filters=self.num_anchors*self.num_classes*self.output_dims, kernel_size=3, padding='same', kernel_initializer='glorot_normal')(act4)
         # TODO this reshape may not actually work
         self.outputs = layers.Reshape((-1, self.num_classes, self.output_dims))(conv5)
 
@@ -53,14 +53,15 @@ class A2J_AnchorProposal(A2J_Regression):
         super().__init__(input_tensor, 1, feature_size=feature_size, num_anchors=num_anchors, num_classes=num_classes)
 
 
-from custom_resnet import resnet, bottleneck_block
-class ResNetBackbone(Model):# LOTS OF TODOS HERE
+from custom_resnet import resnet, bottleneck_block, basic_block
+class ResNetBackbone(Model):
     def __init__(self, input_shape):
         super().__init__(input_shape)
 
     def build(self):
         # input should be channels last
         self.inputs = layers.Input(shape = self.input_shape)
+        # self.outputs = resnet(self.inputs, basic_block, [2,2,2,2])
         self.outputs = resnet(self.inputs, bottleneck_block, [3,4,6,3])
 
         self.model = models.Model(inputs = self.inputs, outputs = self.outputs)
@@ -76,27 +77,31 @@ class A2J(Model):
         self.predict_3D = predict_3D
 
     def build(self):
-        if self.predict_3D:
-            backbone = ResNetBackbone(self.input_shape)
-            backbone.build()
-            print(backbone.model.summary())
-            x3, x4 = backbone.outputs
+        backbone = ResNetBackbone(self.input_shape)
+        backbone.build()
+        print(backbone.model.summary())
+        x3, x4 = backbone.outputs
 
+        if self.predict_3D:
             anchor_proposal = A2J_AnchorProposal(x3, num_classes=self.num_classes)
             anchor_proposal.build()
-            # print(anchor_proposal.model.summary())
 
             in_plane_regression = A2J_InPlaneRegression(x4, num_classes=self.num_classes)
             in_plane_regression.build()
-            # print(in_plane_regression.model.summary())
 
             depth_regression = A2J_DepthRegression(x4, num_classes=self.num_classes)
             depth_regression.build()
-            # print(depth_regression.model.summary())
 
             self.model = models.Model(inputs = backbone.inputs, outputs = [anchor_proposal.outputs, in_plane_regression.outputs, depth_regression.outputs])
+
         else:
-            print('Not yet implemented')
+            anchor_proposal = A2J_AnchorProposal(x3, num_classes=self.num_classes)
+            anchor_proposal.build()
+
+            in_plane_regression = A2J_InPlaneRegression(x4, num_classes=self.num_classes)
+            in_plane_regression.build()
+
+            self.model = models.Model(inputs = backbone.inputs, outputs = [anchor_proposal.outputs, in_plane_regression.outputs])
 
         super().build()
 
@@ -105,5 +110,5 @@ class A2J(Model):
         pass
 
 
-a2j = A2J((360, 480, 1), 15)
+a2j = A2J((256, 256, 1), 15)
 a2j.build()
