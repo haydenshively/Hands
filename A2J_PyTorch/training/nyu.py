a2094207cdf3118d17884fe759225d94311ac029
Hayden Shively <haydenshively@gmail.com>
2020-05-22 10:46:01 -0700

    Trained with just 14 keypoints

--- A2J_PyTorch/training/nyu.py
+++ A2J_PyTorch/training/nyu.py
@@ -78,8 +78,16 @@ joints_to_use[[0, 3, 6, 9, 12, 15, 18, 21, 24, 25, 27, 30, 31, 32]] = True
 # Load coords from train set
 keypointsUVD_train = scio.loadmat(keypoint_file_train)['joint_uvd'].astype(np.float32)
 keypointsUVD__test = scio.loadmat(keypoint_file__test)['joint_uvd'].astype(np.float32)
-keypointsUVD_train = keypointsUVD_train[camera_id - 1, joints_to_use]
-keypointsUVD__test = keypointsUVD__test[camera_id - 1, joints_to_use]
+print(keypointsUVD_train.shape)
+
+keypointsUVD_train = keypointsUVD_train[camera_id - 1]
+keypointsUVD__test = keypointsUVD__test[camera_id - 1]
+print(keypointsUVD_train.shape)
+
+keypointsUVD_train = keypointsUVD_train[:, joints_to_use, :]
+keypointsUVD__test = keypointsUVD__test[:, joints_to_use, :]
+print(keypointsUVD_train.shape)
+
 center_train = keypointsUVD_train.mean(axis=1)
 center__test = keypointsUVD__test.mean(axis=1)
 sizes_train = keypointsUVD_train[:,:,:2].ptp(axis=1).max(axis=1)/2.0 + THRESH_XY
@@ -201,8 +209,8 @@ dataloader__test = torch.utils.data.DataLoader(dataset__test, batch_size=BATCH_S
 
 
 def train(net):
-    post_process = A2JPostProcess(None, None, [IMG_HEIGHT//16, IMG_WIDTH//16], 16)
-    criterion = A2JLoss(None, None, [IMG_HEIGHT//16, IMG_WIDTH//16], 16, SPATIAL_FACTOR)
+    post_process = A2JPostProcess(None, None, [IMG_HEIGHT//32, IMG_WIDTH//32], 16)
+    criterion = A2JLoss(None, None, [IMG_HEIGHT//32, IMG_WIDTH//32], 16, SPATIAL_FACTOR)
     optimizer = torch.optim.Adam(net.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.2)
 
